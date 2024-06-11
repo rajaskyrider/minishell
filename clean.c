deb1b05b92bb6b44911a1aeb5f8bda278fefafec
Raja Pandiperumal <rpandipe@c2r4s5.42luxembourg.lu>
2024-06-11 11:23:43 +0200

    clean

---
+++ clean.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpandipe <rpandipe.student.42luxembourg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 10:57:50 by rpandipe          #+#    #+#             */
+/*   Updated: 2024/06/11 11:17:04 by rpandipe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	delete_token(t_token **token)
+{
+	t_token	*ptr;
+
+	ptr = *token;
+	if (ptr->next)
+		ptr->next->prev = ptr->prev;
+	if (ptr->prev)
+		ptr->prev->next = ptr->next;
+	if (*token == ptr)
+		*token = ptr->next;
+	if (ptr->value)
+		free(ptr->value);
+	free(ptr);
+}
+
+void	delete_token_lst(t_token **token_lst)
+{
+	while (*token_lst)
+		delete_token(token_lst);
+}
+
+void	delete_ast(t_ast **ast)
+{
+	t_ast	*ptr;
+
+	ptr = *ast;
+	if ()
+}
--- utils.c
+++ utils.c
@@ -6,33 +6,11 @@
 /*   By: rpandipe <rpandipe.student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 15:46:03 by rpandipe          #+#    #+#             */
-/*   Updated: 2024/06/11 09:56:50 by rpandipe         ###   ########.fr       */
+/*   Updated: 2024/06/11 11:00:57 by rpandipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	delete_token(t_token **token)
-{
-	t_token	*ptr;
-
-	ptr = *token;
-	if (ptr->next)
-		ptr->next->prev = ptr->prev;
-	if (ptr->prev)
-		ptr->prev->next = ptr->next;
-	if (*token == ptr)
-		*token = ptr->next;
-	if (ptr->value)
-		free(ptr->value);
-	free(ptr);
-}
-
-void	delete_token_lst(t_token **token_lst)
-{
-	while (*token_lst)
-		delete_token(token_lst);
-}
 
 int	init_token(t_token **token)
 {
@@ -52,8 +30,8 @@ void	clear_shell(t_ms *shell)
 		free(shell->cmd);
 	while (shell->token_lst)
 		delete_token(&shell->token_lst);
-	//while (shell->ast)
-	//	delete_ast(&shell->ast);
+	while (shell->ast)
+		delete_ast(&shell->ast);
 }
 
 void	print_error(t_ms *shell, char *errormsg)
