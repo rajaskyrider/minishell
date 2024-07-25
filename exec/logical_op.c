62de1d0de8d3c29a82287e27ca4d469e9188e120
Raja Pandiperumal <rpandipe@c3r7s6.42luxembourg.lu>
2024-07-25 14:54:58 +0200

    subshell issues fixed ?

--- exec/logical_op.c
+++ exec/logical_op.c
@@ -6,7 +6,7 @@
 /*   By: rpandipe <rpandipe.student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:00:58 by rpandipe          #+#    #+#             */
-/*   Updated: 2024/07/25 14:46:08 by rpandipe         ###   ########.fr       */
+/*   Updated: 2024/07/25 14:53:16 by rpandipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	logical_and(t_ast **ast, t_ms **shell, int next_pipe[2])
 {
 	if (!ast || !(*ast))
 		return ;
-	if ((*ast)->left->type == T_OPERATOR || (*ast)->left->token_type == T_PARENT)
+	if ((*ast)->left->type == T_OPERATOR)
 		navigate(&(*ast)->left, shell, next_pipe);
+	else if ((*ast)->left->token_type == T_PARENT)
+			ms_subshell((*ast)->left, *shell);
 	else
 		exec_cmd((*ast)->left, (*ast)->left->value, *shell, 0);
 	close_fd (*shell);
@@ -25,8 +27,10 @@ void	logical_and(t_ast **ast, t_ms **shell, int next_pipe[2])
 	dup2((*shell)->std_out, STDOUT_FILENO);
 	if ((*shell)->lexit_status == 0)
 	{
-		if ((*ast)->right->type == T_OPERATOR || (*ast)->right->token_type == T_PARENT)
+		if ((*ast)->right->type == T_OPERATOR)
 			navigate(&(*ast)->right, shell, next_pipe);
+		else if ((*ast)->right->token_type == T_PARENT)
+			ms_subshell((*ast)->right, *shell);
 		else
 			exec_cmd((*ast)->right, (*ast)->right->value, *shell, 0);
 		close_fd (*shell);
@@ -37,8 +41,10 @@ void	logical_or(t_ast **ast, t_ms **shell, int next_pipe[2])
 {
 	if (!ast || !(*ast))
 		return ;
-	if ((*ast)->left->type == T_OPERATOR || (*ast)->left->token_type == T_PARENT)
+	if ((*ast)->left->type == T_OPERATOR)
 		navigate(&(*ast)->left, shell, next_pipe);
+	else if ((*ast)->left->token_type == T_PARENT)
+			ms_subshell((*ast)->left, *shell);
 	else
 		exec_cmd((*ast)->left, (*ast)->left->value, *shell, 0);
 	close_fd (*shell);
@@ -46,8 +52,10 @@ void	logical_or(t_ast **ast, t_ms **shell, int next_pipe[2])
 	dup2((*shell)->std_out, STDOUT_FILENO);
 	if ((*shell)->lexit_status != 0)
 	{
-		if ((*ast)->right->type == T_OPERATOR || (*ast)->right->token_type == T_PARENT)
+		if ((*ast)->right->type == T_OPERATOR)
 			navigate(&(*ast)->right, shell, next_pipe);
+		else if ((*ast)->right->token_type == T_PARENT)
+			ms_subshell((*ast)->right, *shell);
 		else
 			exec_cmd((*ast)->right, (*ast)->right->value, *shell, 0);
 		close_fd (*shell);
