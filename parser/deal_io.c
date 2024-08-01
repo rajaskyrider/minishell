a713cf54c644190e4127ff71f5f9a14b87d379d8
Thomas Le Moel <tle-moel@c3r6s6.42luxembourg.lu>
2024-08-01 17:44:51 +0200

    fix heredoc order

--- parser/deal_io.c
+++ parser/deal_io.c
@@ -6,7 +6,7 @@
 /*   By: tle-moel <tle-moel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 10:44:08 by rpandipe          #+#    #+#             */
-/*   Updated: 2024/07/31 15:04:42 by tle-moel         ###   ########.fr       */
+/*   Updated: 2024/08/01 17:44:19 by tle-moel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,49 @@ void	create_io(t_token *ptr, t_ms *shell)
 	delete_token(&ptr->next);
 }
 
+void	logic_heredoc_io(t_token *ptr, t_io *io, t_io *temp)
+{
+	if (ptr->io)
+	{
+		temp = ptr->io;
+		while (temp->next && temp->next->type == T_DLESS)
+			temp = temp->next;
+		if (temp->type != T_DLESS)
+		{
+			temp->next = io;
+			io->prev = temp;
+		}
+		else
+		{
+			io->next = temp->next;
+			io->prev = temp;
+			if (temp->next)
+				temp->next->prev = io;
+			temp->next = io;
+		}
+	}
+	else
+		ptr->io = io;
+}
+
+void	add_heredoc_io(t_token *ptr, t_ms *shell)
+{
+	t_io	*io;
+	t_io	*temp;
+
+	io = ft_calloc(sizeof(t_io), 1);
+	if (!io)
+		exit_shell(shell, EXIT_FAILURE);
+	io->type = ptr->next->type;
+	io->value = ft_strdup(ptr->next->next->value);
+	io->next = NULL;
+	io->prev = NULL;
+	temp = NULL;
+	logic_heredoc_io(ptr, io, temp);
+	delete_token(&ptr->next);
+	delete_token(&ptr->next);
+}
+
 void	deal_io(t_ms *shell)
 {
 	t_token	*ptr;
@@ -46,8 +89,10 @@ void	deal_io(t_ms *shell)
 	while (ptr && ptr->next)
 	{
 		if (ptr->next->type == T_LESS || ptr->next->type == T_GREAT || \
-			ptr->next->type == T_DLESS || ptr->next->type == T_DGREAT)
+			ptr->next->type == T_DGREAT)
 			create_io(ptr, shell);
+		else if (ptr->next->type == T_DLESS)
+			add_heredoc_io(ptr, shell);
 		else
 			ptr = ptr->next;
 	}
