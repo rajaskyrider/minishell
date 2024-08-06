8c228d2678ea87f79004d8146db0ed98ed44b211
Raja Pandiperumal <rpandipe@c2r4s5.42luxembourg.lu>
2024-08-06 11:58:56 +0200

    updated logical or logic

--- exec/logical_op.c
+++ exec/logical_op.c
@@ -6,7 +6,7 @@
 /*   By: rpandipe <rpandipe.student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:00:58 by rpandipe          #+#    #+#             */
-/*   Updated: 2024/08/05 15:15:10 by rpandipe         ###   ########.fr       */
+/*   Updated: 2024/08/06 11:55:43 by rpandipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ void	logical_and(t_ast **ast, t_ms **shell, int next_pipe[2])
 	close_fd (*shell);
 	dup2((*shell)->std_in, STDIN_FILENO);
 	dup2((*shell)->std_out, STDOUT_FILENO);
+	wait(NULL);
 	if ((*shell)->lexit_status == 0)
 	{
 		if ((*ast)->right->type == T_OPERATOR)
@@ -37,40 +38,66 @@ void	logical_and(t_ast **ast, t_ms **shell, int next_pipe[2])
 	}
 }
 
+void	print_shlvl(t_ms *shell)
+{
+	t_envlst	*ptr;
+
+	ptr = shell->envlst;
+	while (ms_strcmp(ptr->key , "SHLVL") != 0)
+		ptr = ptr->next;
+	dprintf(2, "SHLVL = %s\n", ptr->value);
+}
+
+void	deal_operator(t_ast **ast, t_ms **shell, int next_pipe[2])
+{
+	int	pid;
+	int child_pid;
+	int	status;
+	
+	pid = fork();
+	if (pid == 0)
+	{
+		navigate(ast, shell, next_pipe);
+		child_pid = wait(&status);
+		while (child_pid > 0)
+		{
+			signal_process();
+			if (WIFEXITED(status))
+				(*shell)->lexit_status = WEXITSTATUS(status);
+			child_pid = wait(&status);
+		}
+		close_fd (*shell);
+		exit((*shell)->lexit_status);
+	}
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+		(*shell)->lexit_status = WEXITSTATUS(status);
+}
+
+void	deal_or(t_ast **ast, t_ms **shell, int next_pipe[2])
+{
+	if ((*ast)->type == T_OPERATOR)
+		deal_operator(&(*ast), shell, next_pipe);
+	else if ((*ast)->token_type == T_PARENT)
+		ms_subshell((*ast), *shell);
+	else
+		exec_cmd((*ast), (*ast)->value, *shell, 0);
+	close_fd (*shell);
+}
+
 void	logical_or(t_ast **ast, t_ms **shell, int next_pipe[2])
 {
 	if (!ast || !(*ast))
 		return ;
-	if ((*ast)->left->type == T_OPERATOR)
-		navigate(&(*ast)->left, shell, next_pipe);
-	else if ((*ast)->left->token_type == T_PARENT)
-		ms_subshell((*ast)->left, *shell);
-	else
-		exec_cmd((*ast)->left, (*ast)->left->value, *shell, 0);
-	close_fd (*shell);
+	deal_or(&(*ast)->left, shell, next_pipe);
 	dup2((*shell)->std_in, STDIN_FILENO);
 	dup2((*shell)->std_out, STDOUT_FILENO);
 	if ((*shell)->lexit_status != 0)
-	{
-		if ((*ast)->right->type == T_OPERATOR)
-			navigate(&(*ast)->right, shell, next_pipe);
-		else if ((*ast)->right->token_type == T_PARENT)
-			ms_subshell((*ast)->right, *shell);
-		else
-			exec_cmd((*ast)->right, (*ast)->right->value, *shell, 0);
-		close_fd (*shell);
-	}
+		deal_or(&(*ast)->right, shell, next_pipe);
 	else if ((*shell)->lexit_status == 0 && \
 		(*ast)->right->token_type == T_AND_IF)
-	{
-		if ((*ast)->right->right->type == T_OPERATOR)
-			navigate(&(*ast)->right->right, shell, next_pipe);
-		else if ((*ast)->right->right->token_type == T_PARENT)
-			ms_subshell((*ast)->right->right, *shell);
-		else
-			exec_cmd((*ast)->right->right, (*ast)->right->right->value, *shell, 0);
-		close_fd (*shell);
-	}
+		deal_or(&(*ast)->right->right, shell, next_pipe);
+
 }
 
 void	deal_logical(t_ast **ast, t_ms **shell, int next_pipe[2])
